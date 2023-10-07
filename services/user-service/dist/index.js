8cdf47a18e991edd85388e961f0b35aaeed795f5
Mayukh Basu <roxy.firebird@gmail.com>
2023-10-07 06:47:30 +0530

    created registration route and tests and added session

--- services/user-service/dist/index.js
+++ services/user-service/dist/index.js
@@ -1,5 +1,28 @@
 "use strict";
 // src/index.ts
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -8,12 +31,12 @@ const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const express_session_1 = __importDefault(require("express-session"));
 const connect_redis_1 = __importDefault(require("connect-redis"));
-const redis_1 = __importDefault(require("redis"));
+const redis = __importStar(require("redis"));
 const auth_1 = __importDefault(require("./routes/auth"));
 const app = (0, express_1.default)();
 const PORT = 3000;
 const RedisStore = (0, connect_redis_1.default)(express_session_1.default);
-const redisClient = redis_1.default.createClient();
+const redisClient = redis.createClient();
 app.use((0, express_session_1.default)({
     store: new RedisStore({ client: redisClient }),
     secret: 'yourSecretKey',
--- services/user-service/types/types.d.ts
+++ services/user-service/types/types.d.ts
@@ -1,7 +1,8 @@
+import session from 'express-session';
 
-// declare module 'express-session' {
-//   interface SessionData {
-//     userId: string; // or whatever type your userId is
-//     // add other custom session properties if needed
-//   }
-// }
+declare module 'express-session' {
+  interface SessionData {
+    userId: string; // or whatever type your userId is
+    // add other custom session properties if needed
+  }
+}
